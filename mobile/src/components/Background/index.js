31782059791063ba24757d9f8ead0a4750b9f016
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-04-05 10:42:57 -0300

    added: background folder

---
+++ mobile/src/components/Background/index.js
@@ -0,0 +1,10 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+// import { Container } from './styles';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#7159c1', '#7D40E7'],
+})`
+  flex: 1;
+`;
