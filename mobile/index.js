d9f54bdebc02c23ae080284d294fc76800b6234d
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-04-05 22:45:23 -0300

    remove file

--- mobile/index.js
+++ mobile/index.js
@@ -1,10 +1,1 @@
-/**
- * @format
- */
-
-import { AppRegistry, YellowBox } from 'react-native';
-import App from './src';
-import { name as appName } from './app.json';
-
-YellowBox.ignoreWarnings(['Calling `getNode()`']);
-AppRegistry.registerComponent(appName, () => App);
+remove file
