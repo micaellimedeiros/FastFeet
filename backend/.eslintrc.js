28703025b0062e633c2719c3f1942311ceab50d1
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-04-09 19:52:26 -0300

    feat: fix problem with prop-types rule

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -21,6 +21,6 @@ module.exports = {
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+    "no-unused-vars": ["error", { "argsIganorePattern": "next" }],
   },
 };
