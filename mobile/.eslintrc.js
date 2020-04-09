20f2e7e83b57f9cd201708a33eaa5e67d4208c0e
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-04-08 22:56:03 -0300

    added: important files to configure

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+    'react-hooks'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js' ] }
+    ],
+    'import/prefer-default-export': 'off',
+    "react/state-in-constructor": "off",
+    'react/static-property-placement':'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    "import/resolver": {
+      "babel-plugin-root-import": {
+        rootPathSuffix: "src"
+      },
+    },
+  }
+};
