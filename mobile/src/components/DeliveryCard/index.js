59a25058ca09ea10a8e35bfcccc4d7362bf40949
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-04-11 19:58:30 -0300

    added: files to deliverycard

---
+++ mobile/src/components/DeliveryCard/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { MaterialCommunityIcons as Icon } from '@expo/vector-icons';
+
+import {
+  Container,
+  Content,
+  Header,
+  Title,
+  Footer,
+  FooterItem,
+  Small,
+  SubTitle,
+  Button,
+  ButtonText,
+} from './styles';
+
+import Timeline from './Timeline';
+
+export default function DeliveryCard({ navigation, delivery }) {
+  return (
+    <Container
+      style={{
+        elevation: 4,
+      }}
+    >
+      <Content>
+        <Header>
+          <Icon size={25} name="truck" color="#7D40E7" />
+          <Title>{`Encomenda ${delivery.formattedId}`}</Title>
+        </Header>
+
+        <Timeline start={delivery.start_date} end={delivery.end_date} />
+      </Content>
+      <Footer>
+        <FooterItem>
+          <Small>Data</Small>
+          <SubTitle>{delivery.formattedDate}</SubTitle>
+        </FooterItem>
+
+        <FooterItem>
+          <Small>Cidade</Small>
+          <SubTitle>{delivery.recipient.city}</SubTitle>
+        </FooterItem>
+
+        <FooterItem>
+          <Button onPress={() => navigation.navigate('Details', { delivery })}>
+            <Small />
+            <ButtonText>Ver detalhes</ButtonText>
+          </Button>
+        </FooterItem>
+      </Footer>
+    </Container>
+  );
+}
+
+DeliveryCard.propTypes = {
+  delivery: PropTypes.shape({
+    formattedId: PropTypes.string.isRequired,
+    start_date: PropTypes.string,
+    end_date: PropTypes.string,
+    formattedDate: PropTypes.string.isRequired,
+    recipient: PropTypes.shape({
+      city: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
